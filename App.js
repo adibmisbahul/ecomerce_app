7e36b673826b00ff17ec7b1cf24be91ff73dbc77
ADIB MISBAHUL ULUM <adibmisbahul101812@gmail.com>
2023-12-10 21:10:33 +0800

    create

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './src/view/home';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F8F8F8',
     alignItems: 'center',
     justifyContent: 'center',
   },
